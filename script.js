c9ea9c043db9d1e3720aef4b700549e9bce12cdc
Itsik300 <Itsik300@users.noreply.github.com>
2026-06-12 22:40:08 +0300

    Add Reve image generator link

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ const DATA=[
     {n:"Kling AI",            u:"https://klingai.com/"},
     {n:"Krea",                u:"https://www.krea.ai/apps/image/flux"},
     {n:"Ideogram",            u:"https://ideogram.ai/t/explore"},
+    {n:"Reve",                u:"https://app.reve.com/home"},
     {n:"ГенерКартинок",       u:"https://app.nichetensor.com/?feature=textToImage"},
     {n:"FLUX.1-pro",          u:"https://glif.app/@001/glifs/clzgvha5a00041aepvz2h4zi4"},
     {n:"FLUX.1-schnell",      u:"https://huggingface.co/spaces/black-forest-labs/FLUX.1-schnell"},
